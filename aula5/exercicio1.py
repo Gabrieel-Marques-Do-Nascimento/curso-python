3055fa3ec32ffa37cbc368b34552ff66102fd842
Gabriel marques do nascimento <142952581+Gabrieel-Marques-Do-Nascimento@users.noreply.github.com>
2024-05-28 14:45:16 -0300

    Update exercicio1.py

--- aula5/exercicio1.py
+++ aula5/exercicio1.py
@@ -1,7 +1,7 @@
 #Exercício Python 16: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
-from math import fabs
+from math import fabs,trunc
 
 num = float(input('digite um numero: '))
-print(f'o numero digitado foi {num} e a sua porcao  inteira e {fabs(num)}')
+print(f'o numero digitado foi {num} e a sua porcâo  inteira e {trunc(num)}')
 
 
